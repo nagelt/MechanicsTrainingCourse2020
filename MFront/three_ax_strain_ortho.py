46332e9583961bba9913037608b08d81a71004ce
Thomas Nagel <nagelt@tcd.ie>
2020-07-16 17:46:16 +0200

    Updated Ortho test.

--- MFront/three_ax_strain_ortho.py
+++ MFront/three_ax_strain_ortho.py
@@ -1,12 +1,13 @@
 import mtest
+import numpy as np
+import matplotlib.pyplot as plt
 
 em = 1.e-2
-npas = 100
-tmax = 1 * 6
+tmax = 6
 m = mtest.MTest()
 mtest.setVerboseMode(mtest.VerboseLevel.VERBOSE_QUIET)
 m.setMaximumNumberOfSubSteps(10)
-m.setBehaviour('generic', 'src/libBehaviour.so', 'MohrCoulombAbboSloanOrtho')
+m.setBehaviour('generic', 'src/libBehaviour.so', 'StandardElasticityBrickOrtho')
 m.setExternalStateVariable("Temperature", 293.15)
 m.setImposedStrain('EXX', {0: 0, tmax / 6: em, tmax / 3: 0})
 m.setImposedStrain('EYY', {tmax / 3: 0, tmax / 2: em, 2 * tmax / 3: 0})
@@ -20,21 +21,43 @@ m.setMaterialProperty('PoissonRatio13', 0.18)
 m.setMaterialProperty('ShearModulus12', 60e3)
 m.setMaterialProperty('ShearModulus23', 30e3)
 m.setMaterialProperty('ShearModulus13', 180e3)
-m.setMaterialProperty('Cohesion', 3.e19)
-m.setMaterialProperty('FrictionAngle', 30.)
-m.setMaterialProperty('DilatancyAngle', 10.)
-m.setMaterialProperty('TransitionAngle', 29.)
-m.setMaterialProperty('TensionCutOffParameter', 1.e1)
 s = mtest.MTestCurrentState()
 wk = mtest.MTestWorkSpace()
 m.completeInitialisation()
 m.initializeCurrentState(s)
 m.initializeWorkSpace(wk)
-ltime = [float((tmax / (npas - 1))) * i for i in range(npas)]
-for i in range(npas - 1):
+ltime = np.linspace(0,tmax,100)
+
+exx = np.array([0.])
+eyy = np.array([0.])
+ezz = np.array([0.])
+sxx = np.array([0.])
+syy = np.array([0.])
+szz = np.array([0.])
+
+
+for i in range(len(ltime) - 1):
     m.execute(s, wk, ltime[i], ltime[i + 1])
-    print(
-        str(s.e1[0]) + " " + str(s.e1[1]) + " " + str(s.e1[2]) + " " +
-        str(s.e1[3]) + " " + str(s.e1[4]) + " " + str(s.e1[5]) + " " +
-        str(s.s1[0]) + " " + str(s.s1[1]) + " " + str(s.s1[2]) + " " +
-        str(s.s1[3]) + " " + str(s.s1[4]) + " " + str(s.s1[5]))
+    exx = np.append(exx,s.e1[0])
+    eyy = np.append(eyy,s.e1[1])
+    ezz = np.append(ezz,s.e1[2])
+    sxx = np.append(sxx,s.s1[0])
+    syy = np.append(syy,s.s1[1])
+    szz = np.append(szz,s.s1[2])
+
+fig,ax = plt.subplots(ncols=2)
+ax[0].plot(ltime,exx*100,label='$\\epsilon_{xx}$')
+ax[0].plot(ltime,eyy*100,label='$\\epsilon_{yy}$')
+ax[0].plot(ltime,ezz*100,label='$\\epsilon_{zz}$')
+ax[0].set_xlabel('$t$ / s')
+ax[0].set_ylabel('$\\epsilon$ / %')
+ax[0].legend()
+
+ax[1].plot(exx*100,sxx,label='$\\epsilon_{xx}-\\sigma{xx}$')
+ax[1].plot(eyy*100,syy,label='$\\epsilon_{yy}-\\sigma{yy}$')
+ax[1].plot(ezz*100,szz,label='$\\epsilon_{zz}-\\sigma{zz}$')
+ax[1].set_xlabel('$\\epsilon$ / %')
+ax[1].legend()
+
+fig.tight_layout()
+fig.savefig('ortho.pdf')
